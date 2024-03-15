e2929296408930d5cfd3bc50de1127e4a360b122
MaxT6 <maximiliant6@gmail.com>
2024-03-15 10:33:20 -0400

    added assets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,9 @@ import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
-    <div className="h-screen">
-      <h1>Threads</h1>
+    <>
+      <h1 className="head-text text-left">Home</h1>
       <UserButton afterSignOutUrl="/" />
-    </div>
+    </>
   )
 }
